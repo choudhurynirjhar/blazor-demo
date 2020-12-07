12c4f6a6ebaae45bfa156859d14647ea1ff1ee4a
Nirjhar Choudhury <nirjhar_choudhury@trimble.com>
2020-12-06 21:36:29 -0500

    Add unit test

---
+++ Blazor.Demo/Pages/Index.razor.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor.Demo.Pages
+{
+    partial class Index
+    {
+        [Inject]
+        private ILoginProcessor LoginProcessor { get; set; }
+
+        private LoginModel login = new();
+        private string errorMessage = string.Empty;
+
+        private bool ValidateEmail()
+        {
+            return !string.IsNullOrEmpty(login.Email);
+        }
+
+        private bool ValidatePassword()
+        {
+            return !string.IsNullOrEmpty(login.Password);
+        }
+
+        private async Task Login()
+        {
+            if(ValidateEmail() && ValidatePassword())
+            {
+                if(!LoginProcessor.Login(login.Email, login.Password))
+                {
+                    errorMessage = "Invalid login";
+                }
+            }
+            else
+            {
+                errorMessage = "Email/Password Invalid";
+            }
+        }
+    }
+}
--- Blazor.Demo/Program.cs
+++ Blazor.Demo/Program.cs
@@ -16,6 +16,8 @@ namespace Blazor.Demo
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<IWeatherProvider, WeatherProvider>();
+            builder.Services.AddScoped<ILoginProcessor, LoginProcessor>();
+
             await builder.Build().RunAsync();
         }
     }

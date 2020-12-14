209fe5f26926a9cb6d5085e4fe5fb6b02cb247ab
Nirjhar Choudhury <nirjhar_choudhury@trimble.com>
2020-12-13 20:53:08 -0500

    Add files and code for JavaScript interop demo

---
+++ Blazor.Demo/Pages/DotnetToJavascript.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace Blazor.Demo.Pages
+{
+    public class DotnetToJavascript : IDotnetToJavascript
+    {
+        private readonly IJSRuntime iJSRuntime;
+        private readonly ILogger<DotnetToJavascript> logger;
+
+        public DotnetToJavascript(IJSRuntime iJSRuntime,
+            ILogger<DotnetToJavascript> logger)
+        {
+            this.iJSRuntime = iJSRuntime;
+            this.logger = logger;
+        }
+
+        public async Task PrintAsync(int counter)
+        {
+            await iJSRuntime.InvokeVoidAsync("logUser", counter);
+        }
+
+        public async Task PrintFormattedMessage()
+        {
+            var message = await iJSRuntime.InvokeAsync<string>("getFormattedMessage");
+            logger.LogInformation(message);
+        }
+    }
+}
---
+++ Blazor.Demo/wwwroot/main.js
@@ -0,0 +1,28 @@
+﻿(function (window) {
+    window.logUser =
+        window.logUser ||
+        function (counter) {
+            console.log(`Printing in JavaScript counter: ${counter}`);
+        }
+
+    window.getFormattedMessage =
+        window.getFormattedMessage ||
+        function () {
+            return "Hello from JavaScript to C#";
+        }
+
+    window.invokeDotnetStaticFunction =
+        window.invokeDotnetStaticFunction ||
+        function () {
+            DotNet.invokeMethodAsync('Blazor.Demo', 'HelpMessage')
+                .then(data => { console.log(data); });
+        }
+
+    window.invokeDotnetInstanceFunction =
+        window.invokeDotnetInstanceFunction ||
+        function (addressProvider) {
+            addressProvider.invokeMethodAsync("GetAddress")
+                .then(data => { console.log(data); });
+        }
+
+})(window);
